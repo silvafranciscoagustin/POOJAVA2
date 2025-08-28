ec95d9f1be85319b60b94c29e3790b305641a403
Francisco Silva <85506323+silvafranciscoagustin@users.noreply.github.com>
2025-08-28 11:34:45 -0300

    Delete _2025_PromoCorta directory

--- _2025_PromoCorta/src/TP_2_1_2025/Main.java
+++
@@ -1,7 +0,0 @@
-package TP_2_1_2025;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
--- _2025_PromoCorta/src/TP_2_2_2025/Main.java
+++
@@ -1,7 +0,0 @@
-package TP_2_2_2025;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
